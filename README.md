27f5a98b89d5623186530bd95276bba38d33e830
Neo <wtzd8645@gmail.com>
2024-11-16 05:23:16 +0800

    fix: Adjust default version format and exception handling

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Used for building an Unity project using a specific installed version. Supports 
 - **unity_install_dir (Optional)**:  Root directory where Unity is installed. The action will search within this directory for the Unity version that matches the project’s requirements. Defaults to the OS-specific Unity installation path.
 - **project_path (Required)**: Absolute path to the Unity project directory that you want to build.
 - **build_method (Required)**: The fully qualified name of the Unity build method to execute.
-- **build_version (Optional)**: Version number for the build. This is used to label the generated logs.
+- **version (Optional)**: Version number for the build. This is used to label the generated logs.
 - **output_dir (Optional)**: Relative path to the project_path where the build output will be stored. Default is "Build/Releases".
 - **log_name (Optional)**: Base name for the build log file. Default is "build_log".
 - **custom_options (Optional)**: Additional command line arguments passed to Unity. Each parameter should start with a dash. If an argument requires a value, place the value directly after the argument, separated by a space.
--- build-unity-project.js
+++ build-unity-project.js
@@ -4,10 +4,14 @@ import { platform } from 'os';
 import { join } from 'path';
 
 function buildUnityProject() {
-  try {
-    const projectPath = process.env.project_path;
-    console.log(`Unity project path used: ${projectPath}`);
+  const projectPath = process.env.project_path;
+  console.log(`Unity project path used: ${projectPath}`);
 
+  const version = process.env.version || new Date().toISOString().slice(0, 19).replace(/[-:]/g, '').replace('T', '.');
+  const logName = process.env.log_name ? `${process.env.log_name}_${version}.log` : `build_log_${version}.log`;
+  const logPath = process.env.output_dir ? join(process.env.output_dir, logName) : logName;
+  let result = 0;
+  try {
     const unityVer = getUnityVersion(projectPath);
     console.log(`Unity version used: ${unityVer}`);
 
@@ -15,153 +19,153 @@ function buildUnityProject() {
     const unityExecutable = getUnityExecutable(unityInstallDir, unityVer);
     console.log(`Executable: ${unityExecutable}`);
 
-    const buildVer = process.env.build_version || new Date().toISOString().slice(0, 19).replace(/[-:]/g, '').replace('T', '-');
-    const logName = process.env.log_name ? `${process.env.log_name}_${buildVer}.log` : `build_log_${buildVer}.log`;
-    const logPath = process.env.output_dir ? join(process.env.output_dir, logName) : logName;
     const args = getBuildArguments(projectPath, logPath);
     console.log("Arguments:", args.join(" "));
-    executeUnityBuild(unityExecutable, args, projectPath);
-
-    console.log("Unity Build Log:");
-    const log = readFileSync(join(projectPath, logPath), 'utf-8');
-    console.log(log);
+    result = executeUnityBuild(unityExecutable, args, projectPath);
   } catch (error) {
     console.error(error.message);
-    process.exit(1);
-  }
-}
-
-function getUnityVersion(projectPath) {
-  try {
-    const versionFilePath = join(projectPath, 'ProjectSettings', 'ProjectVersion.txt');
-    const versionFileContent = readFileSync(versionFilePath, 'utf-8');
-    const versionLine = versionFileContent.split('\n').find(line => line.includes('m_EditorVersion:'));
-    return versionLine.split(' ')[1].trim();
-  } catch (error) {
-    throw new Error(`Failed to parse project version.\n ${error.message}`);
-  }
-}
-
-function getUnityInstallDir(installDir) {
-  if (installDir) {
-    return installDir;
+    result = 1;
+  } finally {
+    console.log("Unity Build Log:");
+    const fullLogPath = join(projectPath, logPath);
+    if (existsSync(fullLogPath)) {
+      console.log(readFileSync(fullLogPath, 'utf-8'));
+    }
+    process.exit(result);
   }
 
-  switch (platform()) {
-    case 'win32':
-      return "C:\\Program Files\\";
-    case 'darwin':
-      return "/Applications/";
-    case 'linux':
-      return "/opt/";
-    default:
-      throw new Error('Unsupported platform.');
+  function getUnityVersion(projectPath) {
+    try {
+      const versionFilePath = join(projectPath, 'ProjectSettings', 'ProjectVersion.txt');
+      const versionFileContent = readFileSync(versionFilePath, 'utf-8');
+      const versionLine = versionFileContent.split('\n').find(line => line.includes('m_EditorVersion:'));
+      return versionLine.split(' ')[1].trim();
+    } catch (error) {
+      throw new Error(`Failed to parse project version.\n ${error.message}`);
+    }
   }
-}
 
-function getUnityExecutable(installDir, version) {
-  const unityDir = findUnityDirectory(installDir, version);
-  if (!unityDir) {
-    console.error("Unable to find the corresponding Unity version installation folder.")
+  function getUnityInstallDir(installDir) {
+    if (installDir) {
+      return installDir;
+    }
+
+    switch (platform()) {
+      case 'win32':
+        return "C:\\Program Files\\";
+      case 'darwin':
+        return "/Applications/";
+      case 'linux':
+        return "/opt/";
+      default:
+        throw new Error('Unsupported platform.');
+    }
+  }
+
+  function getUnityExecutable(installDir, version) {
+    const unityDir = findUnityDirectory(installDir, version);
+    if (!unityDir) {
+      console.error("Unable to find the corresponding Unity version installation folder.")
+      return null;
+    }
+
+    const currPlatform = platform();
+    if (currPlatform === 'win32') {
+      return findUnityExecutable(unityDir, 'Unity.exe');
+    } else if (currPlatform === 'darwin') {
+      return findUnityExecutable(unityDir, 'Unity.app/Contents/MacOS/Unity');
+    } else if (currPlatform === 'linux') {
+      return findUnityExecutable(unityDir, 'Unity');
+    }
     return null;
   }
 
-  const currPlatform = platform();
-  if (currPlatform === 'win32') {
-    return findUnityExecutable(unityDir, 'Unity.exe');
-  } else if (currPlatform === 'darwin') {
-    return findUnityExecutable(unityDir, 'Unity.app/Contents/MacOS/Unity');
-  } else if (currPlatform === 'linux') {
-    return findUnityExecutable(unityDir, 'unity-editor/Unity');
-  }
-  return null;
-}
+  function findUnityDirectory(dir, unityVer) {
+    let queue = [dir];
+    while (queue.length > 0) {
+      const currDir = queue.shift();
 
-function findUnityDirectory(dir, unityVer) {
-  let queue = [dir];
-  while (queue.length > 0) {
-    const currDir = queue.shift();
-
-    let entries;
-    try {
-      entries = readdirSync(currDir, { withFileTypes: true });
-    } catch (error) {
-      if (error.code === 'EPERM' || error.code === 'EACCES') {
-        continue;
+      let entries;
+      try {
+        entries = readdirSync(currDir, { withFileTypes: true });
+      } catch (error) {
+        if (error.code === 'EPERM' || error.code === 'EACCES') {
+          continue;
+        }
+        throw error;
       }
-      throw error;
+
+      for (let i = entries.length - 1; i >= 0; i--) {
+        const entry = entries[i];
+        if (!entry.isDirectory()) {
+          continue;
+        }
+
+        if (entry.name.includes(unityVer)) {
+          return join(currDir, entry.name);
+        }
+
+        queue.push(join(currDir, entry.name));
+      }
+    }
+    return null;
+  }
+
+  function findUnityExecutable(unityDir, executableName) {
+    const filePath = join(unityDir, executableName);
+    if (existsSync(filePath)) {
+      return filePath;
     }
 
-    for (let i = entries.length - 1; i >= 0; i--) {
-      const entry = entries[i];
+    const entries = readdirSync(unityDir, { withFileTypes: true });
+    for (const entry of entries) {
       if (!entry.isDirectory()) {
         continue;
       }
 
-      if (entry.name.includes(unityVer)) {
-        return join(currDir, entry.name);
+      const subDir = join(unityDir, entry.name);
+      const result = findUnityExecutable(subDir, executableName);
+      if (result) {
+        return result;
       }
-
-      queue.push(join(currDir, entry.name));
     }
-  }
-  return null;
-}
-
-function findUnityExecutable(unityDir, executableName) {
-  const filePath = join(unityDir, executableName);
-  if (existsSync(filePath)) {
-    return filePath;
+    return null;
   }
 
-  const entries = readdirSync(unityDir, { withFileTypes: true });
-  for (const entry of entries) {
-    if (!entry.isDirectory()) {
-      continue;
+  function getBuildArguments(projectPath, logPath) {
+    const args = [
+      '-quit',
+      '-batchmode',
+      `-projectPath ${projectPath}`,
+      `-executeMethod ${process.env.build_method}`,
+      `-logFile ${logPath}`
+    ];
+
+    const customOptions = process.env.custom_options || "";
+    const tokens = customOptions ? customOptions.split(" ") : [];;
+    for (let i = 0; i < tokens.length; i++) {
+      const token = tokens[i];
+      if (token.startsWith("-")) {
+        const value = tokens[i + 1] && !tokens[i + 1].startsWith("-") ? tokens[++i] : "";
+        args.push(value ? `${token} ${value}` : token);
+      }
+    }
+    return args;
+  }
+
+  function executeUnityBuild(executable, args, workingDir) {
+    console.log('Start Unity build.');
+    executable = `"${executable}"`;
+    const result = spawnSync(executable, args, { cwd: workingDir, stdio: 'inherit', shell: true });
+    if (result.error) {
+      console.error(`Process failed. ${result.error.message}`);
+      return 1;
     }
 
-    const subDir = join(unityDir, entry.name);
-    const result = findUnityExecutable(subDir, executableName);
-    if (result) {
-      return result;
+    if (result.status !== 0) {
+      console.error(`Process exited with code: ${result.status}`);
     }
-  }
-  return null;
-}
-
-function getBuildArguments(projectPath, logPath) {
-  const args = [
-    '-quit',
-    '-batchmode',
-    `-projectPath ${projectPath}`,
-    `-executeMethod ${process.env.build_method}`,
-    `-logFile ${logPath}`
-  ];
-
-  const customOptions = process.env.custom_options || "";
-  const tokens = customOptions ? customOptions.split(" ") : [];;
-  for (let i = 0; i < tokens.length; i++) {
-    const token = tokens[i];
-    if (token.startsWith("-")) {
-      const value = tokens[i + 1] && !tokens[i + 1].startsWith("-") ? tokens[++i] : "";
-      args.push(value ? `${token} ${value}` : token);
-    }
-  }
-  return args;
-}
-
-function executeUnityBuild(executable, args, workingDir) {
-  console.log('Start Unity build.');
-  executable = `"${executable}"`;
-  const result = spawnSync(executable, args, { cwd: workingDir, stdio: 'inherit', shell: true });
-  if (result.error) {
-    console.error(`Process failed. ${result.error.message}`);
-    process.exit(1);
-  }
-
-  if (result.status !== 0) {
-    console.error(`Process exited with code: ${result.status}`);
-    process.exit(result.status);
+    return result.status
   }
 }
 
